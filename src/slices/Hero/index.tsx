454e545a3e9be5eff2897baaaf68f87a59f25211
W8jonas <jonashenrique989@gmail.com>
2024-06-23 18:11:41 -0300

    Create Bounded

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,3 +1,4 @@
+import Bounded from "@/components/Bounded";
 import { Content } from "@prismicio/client";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
@@ -12,38 +13,33 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="px-4 py-10 md:py-14 md:px-6 lg:py-16"
     >
-      <div className="mx-auto w-full max-w-6xl border">
-
-        <div className="grid grid-cols-1 place-items-center text-center">
-          <PrismicRichText
-            field={slice.primary.heading}
-            components={{
-              heading1: ({children}) => (
-                <h1 className="text-5xl md:text-7xl font-bold leading-tight tracking-tight font-display text-slate-700">{children}</h1>
-              )
-            }}
-          />
-
-          <PrismicRichText field={slice.primary.body_text} components={{
-            paragraph: ({children}) => (
-              <p className="text-2xl text-center font-normal leading-10 font-body text-slate-600 mb-4 md:mb-8 max-w-md">{children}</p>
+      <div className="grid grid-cols-1 place-items-center text-center">
+        <PrismicRichText
+          field={slice.primary.heading}
+          components={{
+            heading1: ({children}) => (
+              <h1 className="text-5xl md:text-7xl font-bold leading-tight tracking-tight font-display text-slate-700">{children}</h1>
             )
-          }} />
+          }}
+        />
 
-          <PrismicNextLink field={slice.primary.button_link} className="block w-fit bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wider mb-8 md:mb-10">
-            {slice.primary.button_label}
-          </PrismicNextLink>
+        <PrismicRichText field={slice.primary.body_text} components={{
+          paragraph: ({children}) => (
+            <p className="text-2xl text-center font-normal leading-10 font-body text-slate-600 mb-4 md:mb-8 max-w-md">{children}</p>
+          )
+        }} />
 
-          <PrismicNextImage field={slice.primary.image} className="drop-shadow-xl max-w-4xl w-full"/>
-        </div>
-        
+        <PrismicNextLink field={slice.primary.button_link} className="block w-fit bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wider mb-8 md:mb-10">
+          {slice.primary.button_label}
+        </PrismicNextLink>
+
+        <PrismicNextImage field={slice.primary.image} className="drop-shadow-xl max-w-4xl w-full"/>
       </div>
-    </section>
+    </Bounded>
   );
 };
 
