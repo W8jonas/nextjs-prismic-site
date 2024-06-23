2b67dbfda6c398898a65f3bcf06ee4cfd5fd90ce
W8jonas <jonashenrique989@gmail.com>
2024-06-23 15:14:58 -0300

    Add metadata from prismic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito, Nunito_Sans } from 'next/font/google'
 import clsx from 'clsx'
+import { createClient } from '@/prismicio'
 
 const nunito = Nunito({
   subsets: ['latin'],
@@ -15,10 +16,20 @@ const nunito_Sans = Nunito_Sans({
   display: 'swap',
 })
 
-export const metadata: Metadata = {
-  title: 'Next Site with Prismic CMS',
-  description: 'Next Site with Prismic CMS, Typescript and Tailwind',
+export async function generateMetadata(): Promise<Metadata> {
+ 
+  const client = createClient()
+  const page = await client.getSingle('settings')
+
+  return {
+    title: page.data.site_title,
+    description: page.data.meta_description,
+    openGraph: {
+      images: [page.data.share_image.url || ''],
+    },
+  }
 }
+
 
 export default function RootLayout({
   children,
@@ -27,7 +38,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={clsx(nunito.variable, nunito_Sans.variable)}>
-      <body>{children}</body>
+      <body>
+        <header>Header!</header>
+        {children}
+        </body>
+        <footer>Footer!</footer>
     </html>
   )
 }
