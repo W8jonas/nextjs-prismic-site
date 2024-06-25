7bcb7b4179f191d8161d9a99c068eaa759112593
W8jonas <jonashenrique989@gmail.com>
2024-06-24 21:27:12 -0300

    Style Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,8 @@
 import { createClient } from "@/prismicio"
 import { PrismicNextLink } from "@prismicio/next"
 import Link from "next/link"
+import Bounded from "./Bounded"
+import { Logo } from "./Logo"
 
 export default async function Header() {
   const client = createClient()
@@ -10,20 +12,27 @@ export default async function Header() {
   const settings = await client.getSingle('settings')
 
   return (
-    <header>
-      <Link href="/">
-        {settings.data.site_title}
-      </Link>
-      
-      <nav>
-        <ul>
-          {settings.data.navigation.map(nav => (
-            <li key={nav.link_label}>
-              <PrismicNextLink field={nav.link}>{nav.link_label}</PrismicNextLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <Bounded as='header' className="py-4 md:py-6 lg:py-8">
+      <div className="flex gap-4 items-center justify-between sm:flex-row">
+        <Link href="/">
+          <Logo/>
+        </Link>
+        
+        <nav>
+          <ul className="flex ">
+            {settings.data.navigation.map(nav => (
+              <li key={nav.link_label}>
+                <PrismicNextLink
+                  field={nav.link}
+                  className="p-3"
+                >
+                  {nav.link_label}
+                </PrismicNextLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </Bounded>
   )
 }
